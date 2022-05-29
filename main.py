5ee5d5b525e18a028852934fae2467e8f5afc027
Kurosu9 <99824788+Kurosu9@users.noreply.github.com>
2022-05-29 13:31:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,80 @@
-from tkinter import *
-from PIL import ImageTk, Image
+import tkinter as tk
+from tkinter import messagebox, PhotoImage
+from PIL import Image, ImageTk
 
-root = Tk()
-root.title("Editor")
-root.iconbitmap("Ala-Too.png")
 
-class logo():
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(Menu)
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+
+class Menu(tk.Frame):
     def __init__(self, master):
-        myFrame = Frame(master)
-        myFrame.pack()
+        tk.Frame.__init__(self, master)
+        self.title = master.title("IMAGE EDITOR")
+        self.im = PhotoImage(file="Ala-Too.png")
+        self.iconphoto = master.iconphoto(False, self.im)
+        # self.resizable = master.resizable(0, 0)
 
-        self.Im = ImageTk.PhotoImage(Image.open("Ala-Too.png"))
-        self.my_label = Label(image=self.Im)
-        self.my_label.pack()
+        self.logo = ImageTk.PhotoImage(Image.open("Ala-Too.png"))
+        tk.Label(self, image=self.logo).pack(side="top")
+        tk.Label(self, text="IMAGE EDITOR", font="Times_New_Roman"
+                 ).pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Add Watermark", font="Comic_Sans_MS",
+                  command=lambda: master.switch_frame(watermark)).pack(pady=10)
+        tk.Button(self, text="Black and White", font="Comic_Sans_MS",
+                  command=lambda: master.switch_frame(style)).pack(pady=10)
+        tk.Button(self, text="Resize ", font="Comic_Sans_MS",
+                  command=lambda: master.switch_frame(resize)).pack(pady=10)
+        tk.Button(self, text="Copyright Section", font="Comic_Sans_MS",
+                  command=self.txt).pack(pady=10)
+        tk.Button(self, text="Exit", font="Comic_Sans_MS",
+                  command=self.exit).pack()
 
-        self.button_quit = Button(master, text="Exite", command=master.quit)
-        self.button_quit.pack()
+    def txt(self):
+        messagebox.showinfo("Copyright", '''Thank you for using this'''
+                            ''' IMAGE EDITOR. @KUROSU9''')
+        self.master()
+
+    def exit(self):
+        choice = messagebox.askyesno("Quit", "Do you want to quit?")
+        if choice:
+            self.master.destroy()
 
 
-k = logo(root)
-root.mainloop()
+class watermark(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="This is page one"
+                 ).pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to Menu", font="Comic_Sans_MS",
+                  command=lambda: master.switch_frame(Menu)).pack()
+
+
+class style(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self).pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to Menu", font="Comic_Sans_MS",
+                  command=lambda: master.switch_frame(Menu)).pack()
+
+
+class resize(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Label(self).pack(side="top", fill="x", pady=10)
+        tk.Button(self, text="Return to Menu", font="Comic_Sans_MS",
+                  command=lambda: master.switch_frame(Menu)).pack()
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
